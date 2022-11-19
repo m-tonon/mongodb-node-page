138816364946e437807532801ce2ef5dcac4881f
m-tonon <math.tonon@gmail.com>
2022-11-19 06:19:27 -0300

    fetching & displaying a list of authors

--- routes/blog.js
+++ routes/blog.js
@@ -1,5 +1,7 @@
 const express = require('express');
 
+const db = require('../data/database')
+
 const router = express.Router();
 
 router.get('/', function(req, res) {
@@ -11,7 +13,11 @@ router.get('/posts', function(req, res) {
 });
 
 router.get('/new-post', async function(req, res) {
-  res.render('create-post');
+  const authors = await db.getDb().collection('authors').find().toArray();
+  // establishing access to the authors collection
+  // '.toArray' get all the authors documents from the collection as an array of data
+
+  res.render('create-post', { authors: authors });
 });
 
 module.exports = router;
