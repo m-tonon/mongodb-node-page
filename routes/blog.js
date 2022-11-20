f83bceb2b8e78a1b5d6ba65e9093ef21db17c83e
m-tonon <math.tonon@gmail.com>
2022-11-20 07:04:10 -0300

    fetching & displaying documents

--- routes/blog.js
+++ routes/blog.js
@@ -8,15 +8,21 @@ const ObjectId = mongodb.ObjectId;
 
 const router = express.Router();
 
-router.get('/', function(req, res) {
+router.get('/', function (req, res) {
   res.redirect('/posts');
 });
 
-router.get('/posts', function(req, res) {
-  res.render('posts-list');
+router.get('/posts', async function (req, res) {
+  const posts = await db
+    .getDb()
+    .collection('posts')
+    .find({})
+    .project({ title: 1, summary: 1, 'author.name': 1 })
+    .toArray();
+  res.render('posts-list', { posts: posts });
 });
 
-router.get('/new-post', async function(req, res) {
+router.get('/new-post', async function (req, res) {
   const authors = await db.getDb().collection('authors').find().toArray();
   // establishing access to the authors collection
   // '.toArray' get all the authors documents from the collection as an array of data
@@ -24,11 +30,14 @@ router.get('/new-post', async function(req, res) {
   res.render('create-post', { authors: authors });
 });
 
-router.post('/posts', async function(req,res){
-  const authorId = new ObjectId(req.body.author.trim()); 
+router.post('/posts', async function (req, res) {
+  const authorId = new ObjectId(req.body.author.trim());
   // holds the object id based on the id submited
   //'.trim()' removes the extra blank space - getting error w/o it
-  const author = await db.getDb().collection('authors').findOne({ _id: authorId }); 
+  const author = await db
+    .getDb()
+    .collection('authors')
+    .findOne({ _id: authorId });
   // filter by the id set on the page
 
   const newPost = {
@@ -39,15 +48,29 @@ router.post('/posts', async function(req,res){
     author: {
       id: authorId, //this submit the value of the option on 'create-post'
       name: author.name, // getting the name from the const author in the filter object
-      email: author.email
-    }
-  }
+      email: author.email,
+    },
+  };
 
   const result = await db.getDb().collection('posts').insertOne(newPost);
   //this create the new post data on mongodb
-  
+
   console.log(result);
   res.redirect('/posts');
+});
+
+router.get('/posts/:id', async function (req, res) {
+  const postId = req.params.id;
+  const post = await db
+    .getDb()
+    .collection('posts')
+    .findOne({ _id: new ObjectId(postId) }, { summary: 0 }); //summary will be exclude from query
+
+  if (!post) {
+    return res.status(404).render('404');
+  }
+
+  res.render('post-detail', { post: post });
 });
 
 module.exports = router;
